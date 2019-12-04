24a2b1a756055bc8b32960f9a427da8b0cba9580
Csaba Gyuris <gycsaba@vasvari.hu>
2019-12-04 09:29:28 +0100

    Dog objektum példányosítása

--- KonstruktorSetGet/Program.cs
+++ KonstruktorSetGet/Program.cs
@@ -25,6 +25,11 @@ namespace KonstruktorSetGet
         /// Konstruktor neve megegyezik az osztály nevével
         /// Konstruktornak nincs visszatérési értéke
         /// Konstroktor kívülről elérhető kell legyen
+
+        /// <summary>
+        /// Dog osztály nevet kérő konstruktora
+        /// </summary>
+        /// <param name="name">A kutya neve</param>
         public Dog(string name)
         {
             this.name = name;
@@ -35,6 +40,8 @@ namespace KonstruktorSetGet
     {
         static void Main(string[] args)
         {
+            //Egy Dog objekutm példányosítása
+            Dog r = new Dog("Rex");
         }
     }
 }
