26c51e724d9f626342dee05b966211ad39b611af
Csaba Gyuris <gycsaba@vasvari.hu>
2019-12-11 09:44:14 +0100

    Car osztály get nélkül elkészült

--- KonstruktorSetGetWorkMethods-Car/Program.cs
+++ KonstruktorSetGetWorkMethods-Car/Program.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 namespace KonstruktorSetGetWorkMethods_Car
 {
+    class Car
+    {
+        private string licensePlate;
+        static int tankCapacity = 60;
+        private int fuelQuantity;
+        static double consumption = 7.8;
+
+        public Car(string licensePlate)
+        {
+            this.licensePlate = licensePlate;
+        }
+        public void tanking(int fuel)
+        {
+            if (fuelQuantity + fuel <= tankCapacity)
+                fuelQuantity += fuel;
+        }
+        public void move(int km)
+        {
+            if (((consumption / 100) * km)< fuelQuantity)
+                 fuelQuantity -= (int) (consumption / 100) * km;
+        }
+    }
+
+
     class Program
     {
+
         static void Main(string[] args)
         {
         }
