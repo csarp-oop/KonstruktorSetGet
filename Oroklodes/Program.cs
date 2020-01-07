23ef0f9d558991a683c9b13b0d9c24f45feae0f3
Csaba Gyuris <gycsaba@vasvari.hu>
2020-01-07 13:21:34 +0100

    Kerekpar

--- Oroklodes/Program.cs
+++ Oroklodes/Program.cs
@@ -18,6 +18,11 @@ namespace Oroklodes
 
         public int X { get => x; set => x = value; }
         public int Y { get => y; set => y = value; }
+
+        public override string ToString()
+        {
+            return "("+x+","+y+")";
+        }
     }
 
     abstract class Jarmu
@@ -27,6 +32,11 @@ namespace Oroklodes
         public Jarmu(int x, int y)
         {
             pozicio = new Pozicio(x, y);
+        }
+
+        public void menjFel()
+        {
+            pozicio.Y = pozicio.Y + 1;
         }
     }
 
@@ -41,13 +51,43 @@ namespace Oroklodes
             this.rendszam = rendszam;
             this.szin = szin;
         }
+
+        public override string ToString()
+        {
+            return 
+                rendszam+" autó"+
+                " amelyik "+szin+" színű "+
+                pozicio+" pozicióban van";
+        }
+    }
+
+    class Kerekpar : Jarmu
+    {
+        private string szin;
+
+        public Kerekpar(int x, int y, string rendszam, string szin)
+            : base(x, y)
+        {
+            this.szin = szin;
+        }
+
+        public override string ToString()
+        {
+            return
+                " Bicikli amelyik " +
+                szin + " színű " +
+                pozicio + " pozicióban van";
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Auto a = new Auto(0, 0, "AAA-001", "rózsaszín");
+            a.menjFel();
+            Console.WriteLine(a);
+            Console.ReadKey();
         }
     }
 }
